6f3a03eb7bd435b43df7509577eb1afeabb8fcf0
One <43485962+c-git@users.noreply.github.com>
2024-01-31 12:02:56 -0500

    Move code for buttons into a function

--- src/app.rs
+++ src/app.rs
@@ -18,6 +18,39 @@ impl BrowseApp {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Default::default()
     }
+
+    fn buttons_save_load(&mut self, ui: &mut egui::Ui) {
+        // a simple button opening the dialog
+        if ui.button("📂 Open text file").clicked() {
+            // Context is wrapped in an Arc so it's cheap to clone as per:
+            // > Context is cheap to clone, and any clones refers to the same mutable data (Context uses refcounting internally).
+            // Taken from https://docs.rs/egui/0.24.1/egui/struct.Context.html
+            let ctx = ui.ctx().clone();
+            self.promise = execute(async move {
+                let file = rfd::AsyncFileDialog::new().pick_file().await?; // Returns None if file is None
+                let text = file.read().await;
+
+                // Uncomment the following line to simulate taking long to load
+                // tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+
+                // If not present screen will not refresh until next paint (comment out to test, works better with the sleep above to demonstrate)
+                ctx.request_repaint();
+
+                Some(String::from_utf8_lossy(&text).to_string())
+            });
+        }
+
+        if ui.button("💾 Save text to file").clicked() {
+            let contents = self.sample_text.clone();
+            self.promise = execute(async move {
+                let file = rfd::AsyncFileDialog::new().save_file().await;
+                if let Some(file) = file {
+                    _ = file.write(contents.as_bytes()).await;
+                }
+                None
+            });
+        }
+    }
 }
 
 impl eframe::App for BrowseApp {
@@ -39,36 +72,7 @@ impl eframe::App for BrowseApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.text_edit_multiline(&mut self.sample_text);
-            // a simple button opening the dialog
-            if ui.button("📂 Open text file").clicked() {
-                // Context is wrapped in an Arc so it's cheap to clone as per:
-                // > Context is cheap to clone, and any clones refers to the same mutable data (Context uses refcounting internally).
-                // Taken from https://docs.rs/egui/0.24.1/egui/struct.Context.html
-                let ctx = ui.ctx().clone();
-                self.promise = execute(async move {
-                    let file = rfd::AsyncFileDialog::new().pick_file().await?; // Returns None if file is None
-                    let text = file.read().await;
-
-                    // Uncomment the following line to simulate taking long to load
-                    // tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
-
-                    // If not present screen will not refresh until next paint (comment out to test, works better with the sleep above to demonstrate)
-                    ctx.request_repaint();
-
-                    Some(String::from_utf8_lossy(&text).to_string())
-                });
-            }
-
-            if ui.button("💾 Save text to file").clicked() {
-                let contents = self.sample_text.clone();
-                self.promise = execute(async move {
-                    let file = rfd::AsyncFileDialog::new().save_file().await;
-                    if let Some(file) = file {
-                        _ = file.write(contents.as_bytes()).await;
-                    }
-                    None
-                });
-            }
+            self.buttons_save_load(ui);
         });
     }
 }
